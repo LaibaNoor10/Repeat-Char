9c9335e1bf11e4903f0ae5b37fe8ba2befce76b4
LaibaNoor10 <125575046+LaibaNoor10@users.noreply.github.com>
2023-05-05 09:31:50 +0530

    Add files via upload

---
+++ RepeatedCharacter[Pr]/RepeatedCharacter[Pr]/Program.cs
@@ -0,0 +1,33 @@
+﻿public class program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name to check repeated characters and its count :");
+        string name = Console.ReadLine();
+
+        Dictionary<char, int> dict = new Dictionary<char, int>();
+        foreach(char ch in name)
+        {
+            if (dict.ContainsKey(ch))
+            {
+                dict[ch]++;
+            }
+            else
+            {
+                dict.Add(ch,1);
+            }
+        }
+
+        foreach (var repeat in dict)
+        {
+            if(repeat.Value>1 && repeat.Key != ' ')
+            {
+                Console.WriteLine("{0} repeats {1} times", repeat.Key, repeat.Value);
+
+            }
+        }
+
+        Console.ReadLine();
+    
+    }
+}
